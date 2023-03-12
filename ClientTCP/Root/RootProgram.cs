be05416739fde0df897452c6de2c4e5288feb82c
Даниил Кабанов <kabanow.dany@yandex.ru>
2023-03-12 20:41:40 +0400

    Классы синглтона и пользователя

---
+++ ClientTCP/Root/RootProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTCP.Root
+{
+    internal class RootProgram
+    {
+        public static RootProgram rootObject;
+
+        
+        RootProgram()
+        {
+        }
+
+        public static RootProgram GetRoot()
+        {
+            if (rootObject == null)
+            {
+                rootObject = new RootProgram();
+            }
+            return rootObject;
+        }
+
+
+    }
+}
